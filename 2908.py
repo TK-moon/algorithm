bfb85e8b8b659d2db3265dd9b9c7791127a37ccf
TK-moon <tkmoon.dev@gmail.com>
2021-05-01 20:51:05 +0900

    2908 refactoring

--- 2908.py
+++ 2908.py
@@ -1,12 +1,19 @@
+# a, b = input().split()
+
+# a = list(a)
+# b = list(b)
+
+# a.reverse()
+# b.reverse()
+
+# a = int(''.join(a))
+# b = int(''.join(b))
+
+# print(max([a, b]))
+
 a, b = input().split()
 
-a = list(a)
-b = list(b)
-
-a.reverse()
-b.reverse()
-
-a = int(''.join(a))
-b = int(''.join(b))
+a = int(''.join(list(reversed(list(a)))))
+b = int(''.join(list(reversed(list(b)))))
 
 print(max([a, b]))
