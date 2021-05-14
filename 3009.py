6e6d7de8bb7d8b6bfbf1217f8125c376ca2fd5d3
TK-moon <tkmoon.dev@gmail.com>
2021-05-14 21:56:31 +0900

    네 번째 점 리팩토링

--- 3009.py
+++ 3009.py
@@ -1,32 +1,47 @@
+# xList = []
+# yList = []
+
+# for i in range(3):
+# 	x, y = map(int, input().split())
+# 	xList.append(x)
+# 	yList.append(y)
+
+# seenX = set()
+# dupX = 0
+# for num in xList:
+# 	if num in seenX:
+# 		dupX = num
+# 		break
+# 	else: seenX.add(num)
+
+# seenY = set()
+# dupY = 0
+# for num in yList:
+# 	if num in seenY:
+# 		dupY = num
+# 		break
+# 	else: seenY.add(num)
+
+# for num in xList:
+# 	if num != dupX:
+# 		x = num
+# for num in yList:
+# 	if num != dupY:
+# 		y = num
+
+# print(x, y)
+
+
 xList = []
 yList = []
 
 for i in range(3):
 	x, y = map(int, input().split())
-	xList.append(x)
-	yList.append(y)
+	
+	if x in xList: xList.remove(x)
+	else: xList.append(x)
 
-seenX = set()
-dupX = 0
-for num in xList:
-	if num in seenX:
-		dupX = num
-		break
-	else: seenX.add(num)
+	if y in yList: yList.remove(y)
+	else: yList.append(y)
 
-seenY = set()
-dupY = 0
-for num in yList:
-	if num in seenY:
-		dupY = num
-		break
-	else: seenY.add(num)
-
-for num in xList:
-	if num != dupX:
-		x = num
-for num in yList:
-	if num != dupY:
-		y = num
-
-print(x, y)
+print(xList[0], yList[0])
