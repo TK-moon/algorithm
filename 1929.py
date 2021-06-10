7a22f281fe622568248619a920f8da5379757215
TK-moon <tkmoon.dev@gmail.com>
2021-06-10 19:41:04 +0900

    4153 직각삼각형 and Formatter 설치

--- 1929.py
+++ 1929.py
@@ -2,11 +2,16 @@ import math
 
 m, n = map(int, input().split())
 
+
 def isPrime(num):
-	if num == 1: return False
-	for i in range(2, int(math.sqrt(num)) + 1):
-		if num % i == 0: return False
-	return True;
+    if num == 1:
+        return False
+    for i in range(2, int(math.sqrt(num)) + 1):
+        if num % i == 0:
+            return False
+    return True
+
 
 for i in range(m, n + 1):
-	if isPrime(i): print(i)
+    if isPrime(i):
+        print(i)
