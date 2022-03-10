e82ba0fc958bb7138213449b3bc8beeb4e497c46
TK-moon <tkmoon.dev@gmail.com>
2022-03-10 15:20:07 +0900

    인터넷 풀이 예시 추가

--- 2480.py
+++ 2480.py
@@ -30,3 +30,22 @@ else:
 #     print(1000+b*100)
 # else:
 #     print(100 * max(a,b,c))
+
+
+
+# 인터넷 풀이 2
+## set을 활용
+
+# temp=input().split(" ") # 값을 받아옴(문자열)
+# list=[]
+# for i in temp:
+#     list.append(int(i)) # 정수형으로 바꿔서 list라는 배열에 넣음
+# list.sort() # 리스트 정렬
+# list_s = set(list) # 리스트를 집합화 시켜주고 list_s에 넣어줌
+
+# if len(list_s)==1 : # 모두 같은눈이 나왔을 때
+#     print(10000+list[0]*100)
+# elif len(list_s)==3 : # 모두 다른눈이 나왔을 때
+#     print(list[-1]*100)
+# else : # 2개가 같은눈이 나왔을 때
+#     print(1000+list[1]*100)  # 총 3개의 원소 중 정렬을 했을 때 중간이 같은수
