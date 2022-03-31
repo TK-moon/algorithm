5ffb89ce40949b866caf0c1c0518892af44cc576
TK-moon <tkmoon.dev@gmail.com>
2022-03-31 15:33:01 +0900

    이진 탐색

---
+++ baekjoon/1920.py
@@ -0,0 +1,20 @@
+n1 = int(input())
+l1 = list(map(int, input().split()))
+l1.sort()
+
+n2 = int(input())
+l2 = list(map(int, input().split()))
+
+def bin_search(l, target, start, end):
+  while start <= end:
+    mid = (start + end) // 2
+    if l[mid] == target: return True
+    if l[mid] > target:
+      end = mid - 1
+    else:
+      start = mid + 1
+  return False
+
+for i in l2:
+  result = bin_search(l1, i, 0, n1 - 1)
+  print(1 if result else 0)
