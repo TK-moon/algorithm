c9a5b52b5ee7a057474f437e410a62fb8b39da2c
TK-moon <tkmoon.dev@gmail.com>
2022-04-03 19:07:39 +0900

    별찍기 개선

--- programmers/직사각형_별찍기.py
+++ programmers/직사각형_별찍기.py
@@ -1,5 +1,12 @@
 n, m = map(int, input().split())
 
-for i in range(m):
-    print(''.join(['*' for j in range(n)]))
-        
+# for i in range(m):
+#   for j in range(n):
+#     print('*', end='')
+#   print()
+
+# for i in range(m):
+#     print('*' * n)
+
+answer = ('*' * n +'\n') * m
+print(answer)
