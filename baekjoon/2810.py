42f959806b2414052e653ff3b5758b9f5fcff7f0
TK-moon <tkmoon.dev@gmail.com>
2022-04-24 22:41:04 +0900

    백준/2810 개선

--- baekjoon/2810.py
+++ baekjoon/2810.py
@@ -1,15 +1,23 @@
+# n = int(input())
+# line = input()
+
+# cnt = 1
+# i = 0
+
+# while i < len(line):
+#   if line[i] == 'S':
+#     cnt += 1
+#     i+= 1
+#   elif line[i] == 'L':
+#     cnt += 1
+#     i += 2
+
+# print(min(n, cnt))
+
 n = int(input())
 line = input()
 
-cnt = 1
-i = 0
-
-while i < len(line):
-  if line[i] == 'S':
-    cnt += 1
-    i+= 1
-  elif line[i] == 'L':
-    cnt += 1
-    i += 2
-
-print(min(n, cnt))
+if line.count('LL') > 0:
+  print(len(line.replace('LL', 'S')) + 1)
+else:
+  print(n)
