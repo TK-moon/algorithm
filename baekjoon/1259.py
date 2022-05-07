a453a33c17697e088437977a2fd4b6c4337098e4
TK-moon <tkmoon.dev@gmail.com>
2022-05-07 17:25:53 +0900

    파이썬 1259

---
+++ baekjoon/1259.py
@@ -0,0 +1,14 @@
+while True:
+  n = input()
+  if n == '0': break
+
+  repeat = len(n) // 2
+  if len(n) & 1: repeat += 1
+
+  flag = 'yes'
+  for i in range(repeat):
+    a = n[i]
+    b = n[len(n)-(i+1)]
+    if a != b: flag = 'no'
+
+  print(flag)
