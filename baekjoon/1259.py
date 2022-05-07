2c889941a91be8fd0cd99df6ecf930061c2462bc
TK-moon <tkmoon.dev@gmail.com>
2022-05-07 17:27:42 +0900

    백준 1259 리팩토링

--- baekjoon/1259.py
+++ baekjoon/1259.py
@@ -12,3 +12,12 @@ while True:
     if a != b: flag = 'no'
 
   print(flag)
+
+# 리팩토링
+## 뒤집어서 똑같으면 펠린드롬 수
+while True:
+  n = input()
+  if n == '0': break
+  
+  if n == n[::-1]: print('yes')
+  else: print('no')
