3639294b327258db7818f98f22eaadab47e499c2
TK-moon <tkmoon.dev@gmail.com>
2022-05-08 22:16:45 +0900

    백준 5533 리팩토링

--- baekjoon/5533.py
+++ baekjoon/5533.py
@@ -1,19 +1,17 @@
 players = int(input())
 
-table = []
+games = [[], [], []]
 score = [0] * players
 
 for i in range(players):
-  table.append(list(map(int, input().split())))
+  temp = list(map(int, input().split()))
+  for i in range(3):
+    games[i].append(temp[i])
 
 for term in range(3):
-  games = []
-  for player in range(players):
-    games.append(table[player][term])
-
   for i in range(players):
-    if games.count(games[i]) == 1:
-      score[i] += games[i]
+    if games[term].count(games[term][i]) == 1:
+      score[i] += games[term][i]
 
 for i in score:
   print(i)
